0ca1fed1e350e9e5105f55ccabfac7b9deafff12
Jonas Pannee <jonpan1204@skola.goteborg.se>
2021-11-01 14:20:48 +0100

    Changed control scheme and added some more prefabs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private SpriteRenderer spriteRend;
-    public float speed = 15;
+    public float speed = 3000;
     public bool grounded;
     public bool crouch;
     Rigidbody rbPlayer;
@@ -21,18 +21,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        transform.Translate(direction.normalized * Time.deltaTime * speed);
+        rbPlayer.AddForce(direction.normalized * Time.deltaTime * speed);
 
-        //float x = Input.GetAxisRaw("Horizontal");
-        //float z = Input.GetAxisRaw("Vertical");
 
-        //movement.x = x * speed;
-        //movement.z = z * speed;
         
-        //movement = rbPlayer.velocity;
-        //rbPlayer.velocity = movement;
 
     }
 
